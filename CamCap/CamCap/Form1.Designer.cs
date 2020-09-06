dfcf09ac2adbb291388aa489d58b05ce5f21b04c
keiya shimomura <skeiya@gmail.com>
2020-09-06 12:13:29 +0900

    CamCap追加。

---
+++ CamCap/CamCap/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CamCap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(440, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(419, 19);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "C:\\Users\\skeiya\\caps";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 521);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ CamCap/CamCap/Form1.cs
@@ -0,0 +1,118 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CamCap
+{
+    public partial class Form1 : Form
+    {
+        int WIDTH = 640;
+        int HEIGHT = 480;
+        Mat frame;
+        VideoCapture capture;
+        Bitmap bmp;
+        Graphics graphic;
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += Form1_FormClosing;
+            this._timer.Tick += _timer_Tick;
+            //カメラ画像取得用のVideoCapture作成
+            capture = new VideoCapture(0);
+            if (!capture.IsOpened())
+            {
+                MessageBox.Show("camera was not found!");
+                this.Close();
+            }
+            capture.FrameWidth = WIDTH;
+            capture.FrameHeight = HEIGHT;
+
+            //取得先のMat作成
+            frame = new Mat(HEIGHT, WIDTH, MatType.CV_8UC3);
+
+            //表示用のBitmap作成
+            bmp = new Bitmap(frame.Cols, frame.Rows, (int)frame.Step(), System.Drawing.Imaging.PixelFormat.Format24bppRgb, frame.Data);
+
+            //PictureBoxを出力サイズに合わせる
+            pictureBox1.Width = frame.Cols;
+            pictureBox1.Height = frame.Rows;
+
+            //描画用のGraphics作成
+            graphic = pictureBox1.CreateGraphics();
+
+            //画像取得スレッド開始
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //描画
+            graphic.DrawImage(bmp, 0, 0, frame.Cols, frame.Rows);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bw = (BackgroundWorker)sender;
+
+            while (!backgroundWorker1.CancellationPending)
+            {
+                //画像取得
+                capture.Grab();
+                NativeMethods.videoio_VideoCapture_operatorRightShift_Mat(capture.CvPtr, frame.CvPtr);
+
+                bw.ReportProgress(0);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //スレッドの終了を待機
+            backgroundWorker1.CancelAsync();
+            while (backgroundWorker1.IsBusy)
+                Application.DoEvents();
+        }
+
+        Timer _timer = new Timer();
+        Stopwatch _sw = new Stopwatch();
+
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            var path = Path.Combine(textBox1.Text, DateTime.Now.ToString("yyyyMMddhhmmssfff")) + ".jpg";
+            frame.SaveImage(path);
+            //using (Mat cap = new Mat(@"C:\cs_source\img\cap.png"))
+            //{
+            //    //保存されたキャプチャ画像の出力
+            //    Cv2.ImShow("test1", frame);
+            //}
+
+            if (_sw.Elapsed.TotalSeconds > 15)
+            {
+                _sw.Stop();
+                _timer.Enabled = false;
+                button1.Enabled = true;
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _timer.Interval = 50;
+            _timer.Enabled = true;
+            _sw.Restart();
+            button1.Enabled = false;
+        }
+    }
+}
